6cef25a1a5df7f6e81074df57eeea714d1ac1314
Konstantin Petrov <k.petrov@klerk.ru>
2024-06-14 18:39:34 +0300

    Errors handling for API methods

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,18 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         using: function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        },
-        commands: __DIR__.'/../routes/console.php',
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    },
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -23,5 +24,31 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (
+            \Symfony\Component\HttpKernel\Exception\HttpException $e,
+            \Illuminate\Http\Request $request
+        ) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $e->getMessage(),
+                ], $e->getStatusCode());
+            }
+        })->render(function (
+            Illuminate\Validation\ValidationException $e
+        ) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 422);
+        })->render(function (
+            \Exception $e
+        ) {
+            // General exceptions need to be logged
+            Log::error($e->getMessage());
+            return response()->json([
+                'success' => false,
+                'message' => 'An internal error occurred. See the logs for the details',
+            ], 400);
+        });
     })->create();
